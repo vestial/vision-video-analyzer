2b867be16a8b767358a81685dfcab62fe0af3165
Penske Williano <penskewilliano@gmail.com>
2020-10-23 21:59:08 +0200

    Add home page

--- main/views.py
+++ main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>Testing</h1>")
+def home(response):
+	return render(response, "main/home.html", {})
