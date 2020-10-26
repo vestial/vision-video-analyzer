43176c986d2c265a2ef228674c2c7108af6f4b3a
Penske Williano <penskewilliano@gmail.com>
2020-10-26 16:45:41 +0100

    Set up initial video model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Video(models.Model):
+    name = models.CharField(max_length=100)
+    uploader = ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
