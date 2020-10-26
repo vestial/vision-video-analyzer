80bfeec515fa78e12318a4793106c6d4031b5daf
Penske Williano <penskewilliano@gmail.com>
2020-10-26 10:51:09 +0100

    Add videos page base

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("videos/", views.videos, name="videos"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(response):
 	return render(response, "main/home.html", {})
+
+def videos(response):
+	return render(response, "main/videos.html", {})
