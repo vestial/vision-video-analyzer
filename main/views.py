95f0eec1de73ef635cf45f2c05a22aa3db4e6478
Penske Williano <penskewilliano@gmail.com>
2020-10-26 12:00:30 +0100

    Add basic file upload functionality

--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
+from django.http import request
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def home(response):
+	if response.method == "POST":
+		uploaded_file = response.FILES['document']
+		fs = FileSystemStorage()
+		fs.save(uploaded_file.name, uploaded_file)
 	return render(response, "main/home.html", {})
 
 def videos(response):
 	return render(response, "main/videos.html", {})
+
--- vision_video_analyzer/settings.py
+++ vision_video_analyzer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
