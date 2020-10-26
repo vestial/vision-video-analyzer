8f2318f695c6dfff07014222ba202f63ea32d54d
Penske Williano <penskewilliano@gmail.com>
2020-10-26 12:30:08 +0100

    Add uploaded file type check

--- main/views.py
+++ main/views.py
@@ -1,14 +1,19 @@
 from django.http import request
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.contrib import messages
 
 # Create your views here.
 def home(response):
 	if response.method == "POST":
 		uploaded_file = response.FILES['document']
-		print(uploaded_file.content_type)
-		fs = FileSystemStorage()
-		fs.save(uploaded_file.name, uploaded_file)
+		if uploaded_file.content_type[:5] == "video":
+			print("upload successful")
+			messages.success(response, "The video was uploaded successfully.")
+			fs = FileSystemStorage()
+			fs.save(uploaded_file.name, uploaded_file)
+		else:
+			messages.error(response, "The uploaded file is not a video!")
 	return render(response, "main/home.html", {})
 
 def videos(response):
