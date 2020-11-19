15ac88f2d011d4ef31d47262b65248dec12acaa2
Penske Williano <penskewilliano@gmail.com>
2020-11-19 20:59:34 +0100

    Fix favicon error bug

--- vision_video_analyzer/settings.py
+++ vision_video_analyzer/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
@@ -132,3 +135,4 @@ LOGOUT_REDIRECT_URL = "/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
 
+SITE_URL = "http://127.0.0.1:8000"
