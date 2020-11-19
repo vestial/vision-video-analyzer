6ee0a42cd2aeb4f478dbad561ef881e98c60a0bb
Penske Williano <penskewilliano@gmail.com>
2020-11-19 22:14:08 +0100

    Add characteristic rating to models and view

--- main/models.py
+++ main/models.py
@@ -15,30 +15,37 @@ class Video(models.Model):
 
     #Resolution 
     resolution = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
+    resolution_rating = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
     resolution_recommendation = models.TextField(blank=True, null=True, default="No recommendation available.")
 
     #Shutter speed
     shutter_speed = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
+    shutter_speed_rating = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
     shutter_speed_recommendation = models.TextField(blank=True, null=True, default="No recommendation available.")
 
     #Frame rate
     frame_rate = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
+    frame_rate_rating = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
     frame_rate_recommendation = models.TextField(blank=True, null=True, default="No recommendation available.")
 
     #Bit rate
     bit_rate = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
+    bit_rate_rating = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
     bit_rate_recommendation = models.TextField(blank=True, null=True, default="No recommendation available.")
 
     #Bit depth
     bit_depth = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
+    bit_depth_rating = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
     bit_depth_recommendation = models.TextField(blank=True, null=True, default="No recommendation available.")
 
     #Sample rate
     sample_rate = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
+    sample_rate_rating = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
     sample_rate_recommendation = models.TextField(blank=True, null=True, default="No recommendation available.")
 
     #Video length
     video_length = models.DurationField(blank=True, null=True)
+    video_length_rating = models.CharField(max_length=64, blank=True, null=True, default="Unknown")
     video_length_recommendation = models.TextField(blank=True, null=True, default="No recommendation available.")
 
     def __str__(self):
