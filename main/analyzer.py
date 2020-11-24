56731990e9d767f16267f7efb75efef35e64705f
Penske Williano <penskewilliano@gmail.com>
2020-11-24 20:01:50 +0100

    Add bit rate analyzer

--- main/analyzer.py
+++ main/analyzer.py
@@ -21,7 +21,7 @@ def get_resolution(video):
                                  'stream=width,height', '-of', 'csv=s=x:p=0 ', video_input_path], capture_output=True, text=True, input="Y")
     return resolution.stdout
 
-#Get fps from ffprobe in frame/sec format. Rounded to make it look better
+# Get fps from ffprobe in frame/sec format. Rounded to make it look better
 def get_frame_rate(video):
     video_input_path = f'{videos}/{video}'
     frame_rate = subprocess.run(['ffprobe', '-v', '0', '-of', 'csv=p=0', '-select_streams', 'v:0',
@@ -29,5 +29,13 @@ def get_frame_rate(video):
     fps_string = frame_rate.stdout
     a = int(fps_string.split('/')[0])
     b = int(fps_string.split('/')[1])
-    rounded_fps = int(np.round(np.divide(a,b)))
+    rounded_fps = int(np.round(np.divide(a, b)))
     return rounded_fps
+
+# Get bit rate from ffprobe and format it to kbps
+def get_bit_rate(video):
+    video_input_path = f'{videos}/{video}'
+    bit_rate = subprocess.run(['ffprobe', '-v', '0', '-select_streams', 'a:0', '-show_entries', 'stream=bit_rate',
+                    '-of', 'compact=p=0:nk=1', video_input_path], capture_output=True, text=True, input="Y")
+    rounded_bit_rate = str(np.round(np.divide(int(bit_rate.stdout), 1000))) + " kbps"
+    return rounded_bit_rate
