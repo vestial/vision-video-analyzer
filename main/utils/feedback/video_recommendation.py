adf919e38a8c8270a9c785c4f4e7218bae1ed705
Penske Williano <penskewilliano@gmail.com>
2021-01-28 00:45:26 +0100

    Fix some bugs on variable assignments

--- main/utils/feedback/video_recommendation.py
+++ main/utils/feedback/video_recommendation.py
@@ -8,7 +8,7 @@ def get_resolution_recommendation(video):
     resolution = list(map(int, resolution))
     if resolution[1] >= 1080 and resolution[1] <= 2160:  #Between 1080p and 4k
         rating = "Great!"
-        resolution = "Great video resolution!"
+        recommendation = "Great video resolution!"
     elif resolution[1] >= 720 and resolution[1] < 1080:  #Between 720p and 1080p
         rating = "Okay"
         recommendation = "Your resolution is okay. However, you can still improve it by increasing the resolution to 1080p."
@@ -16,8 +16,8 @@ def get_resolution_recommendation(video):
         rating = "Bad"
         recommendation = "Your resolution is too low. Please increase it to at least 720p."
     else:
-        result = "Okay"
-        resolution = "Your video resolution might be too high. Try lowering it to at least 4k to increase support for more devices and reduce bandwidth usage."
+        rating = "Okay"
+        recommendation = "Your video resolution might be too high. Try lowering it to at least 4k to increase support for more devices and reduce bandwidth usage."
     return (rating, recommendation)
 
 
