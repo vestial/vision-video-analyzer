3b67814bbccea1a058e6f60b6dc12a836f159b9f
Penske Williano <penskewilliano@gmail.com>
2021-01-28 22:37:22 +0100

    Add shot length recommendation

--- main/utils/feedback/shot/shot_length.py
+++ main/utils/feedback/shot/shot_length.py
@@ -10,9 +10,18 @@ def get_shot_length_recommendation_wrapper(shot_lengths):
 
 def get_shot_length_recommendation(shot_length):
 
-    results = [
-        "Shot length recommended", "Shot length rating",
-        " Shot length feedback"
-    ]
+    recommended = "Between 15 and 30 seconds."
+    rating = ""
+    feedback = ""
 
-    return results
+    if (shot_length >= 15 and shot_length <= 30):
+        rating = "Great!"
+        feedback = "The length of your shot is great, since it is between 15 and 30 seconds."
+    elif (shot_length < 15):
+        rating = "Bad"
+        feedback = "Try increasing the length of your shot to at least 15 seconds to enable audience to understand the information presented."
+    else:
+        rating = "Bad"
+        feedback = "Try reducing the length of your shot to at least 30 seconds. Avoid too long shots that the audience cannot understand."
+
+    return [recommended, rating, feedback]
