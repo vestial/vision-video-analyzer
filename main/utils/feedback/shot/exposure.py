78522a24137db572719e6b4df93b00ee06db318d
Penske Williano <penskewilliano@gmail.com>
2021-01-28 23:05:32 +0100

    Add exposure feedback algorithm

--- main/utils/feedback/shot/exposure.py
+++ main/utils/feedback/shot/exposure.py
@@ -10,6 +10,21 @@ def get_exposure_recommendation_wrapper(exposures):
 
 def get_exposure_recommendation(exposure):
 
-    results = ["Exposure recommended", "Exposure rating", " Exposure feedback"]
+    recommended = "Between 15 and 30 seconds."
+    rating = ""
+    feedback = ""
 
-    return results
+    if (exposure == "Shot is underexposed!"):
+        rating = "Bad"
+        feedback = "Try increasing the brightness of the shot by using brighter lights and using lighter colored backgrounds."
+    elif (exposure == "Shot is overexposed!"):
+        rating = "Bad"
+        feedback = "Try reducing the brightness of the shot by avoiding strong lights and using darker colored backgrounds."
+    elif (exposure == "Exposure is normal."):
+        rating = "Great"
+        feedback = "Your exposure is already great!"
+    else:
+        rating = "Bad"
+        feedback = "Try increasing the brightness of the underexposed/dark shot by using brighter lights and using lighter colored backgrounds. Also, Try reducing the brightness of the overexposed/bright shot by avoiding strong lights and using darker colored backgrounds."
+
+    return [recommended, rating, feedback]
