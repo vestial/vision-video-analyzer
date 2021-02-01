1a193db4c255d676937262303bbe6dcad9ba936f
Penske Williano <penskewilliano@gmail.com>
2021-02-01 13:00:35 +0100

    Fix some typo and set a contrast value for RMS

--- main/utils/feedback/shot/contrast.py
+++ main/utils/feedback/shot/contrast.py
@@ -10,6 +10,18 @@ def get_contrast_recommendation_wrapper(contrasts):
 
 def get_contrast_recommendation(contrast):
 
-    results = ["Contrast recommended", "Contrast rating", " Contrast feedback"]
+    recommended = "Between 15 and 30 seconds."
+    rating = ""
+    feedback = ""
+    #Temporary set values
+    if (contrast >= 0.3):
+        rating = "Great!"
+        feedback = "The contrast of your shot is great!"
+    elif (contrast < 0.3):
+        rating = "Bad"
+        feedback = "Try to increase your RMS contrast to at least 0.3. You can do this by either changing your settings or by choosing video backgrounds with colors which contrast with the subject. "
+    else:
+        rating = "Unknown"
+        feedback = "Unknown"
 
-    return results
+    return [recommended, rating, feedback]
