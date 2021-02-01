f0a319305355bf1b4d71cfe2b2d3eeb5f0ca0bb0
Penske Williano <penskewilliano@gmail.com>
2021-02-01 23:15:08 +0100

    Fix contrast recommendation

--- main/utils/feedback/shot/contrast.py
+++ main/utils/feedback/shot/contrast.py
@@ -10,18 +10,18 @@ def get_contrast_recommendation_wrapper(contrasts):
 
 def get_contrast_recommendation(contrast):
 
-    recommended = "Between 15 and 30 seconds."
+    recommended = "Between 0.3 and 0.8."
     rating = ""
     feedback = ""
     #Temporary set values
-    if (contrast >= 0.3):
+    if (contrast >= 0.3 and contrast <= 0.8):
         rating = "Great!"
         feedback = "The contrast of your shot is great!"
     elif (contrast < 0.3):
         rating = "Bad"
         feedback = "Try to increase your RMS contrast to at least 0.3. You can do this by either changing your settings or by choosing video backgrounds with colors which contrast with the subject. "
     else:
-        rating = "Unknown"
-        feedback = "Unknown"
+        rating = "Bad"
+        feedback = "Try to decrease your RMS contrast to at least 0.8. You can do this by either changing your settings or by choosing video backgrounds with colors which contrast with the subject. "
 
     return [recommended, rating, feedback]
